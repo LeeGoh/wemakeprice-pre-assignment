a35b19ebe13cd6bcaca76b713bb056c4ec395517
LeeGoh <gyuleegoh@gmail.com>
2023-03-03 00:30:14 +0900

    set: 프로젝트 초기 환경 세팅

---
+++ wemakeprice/src/test/java/com/preassignment/wemakeprice/WemakepriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preassignment.wemakeprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WemakepriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
